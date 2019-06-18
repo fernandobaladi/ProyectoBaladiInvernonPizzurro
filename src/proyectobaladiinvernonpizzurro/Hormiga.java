fd92542a5ffb25db6c1a84604298ef1916783661
FernandoBaladi <fernandobaladit@gmail.com>
2019-06-17 20:35:18 -0400

    Commit sin sentido y porque el puto git me lo pide:

---
+++ src/proyectobaladiinvernonpizzurro/Hormiga.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectobaladiinvernonpizzurro;
+
+/**
+ *
+ * @author Fernando Baladi
+ */
+public class Hormiga {
+    public int nCiudades; //Al saber el número de ciudades se pude deducir cuántas faltan por recorrer
+    public int[] cRecorridas; //Al saber cuáles ciudades se recorrieron se puede saber cuáles quedan por recorrer
+    public NodoVertice ciudadInicial;
+
+    public Hormiga(NodoVertice ciudadInicial, int nCiudades) {
+        this.ciudadInicial = ciudadInicial;
+        this.nCiudades = nCiudades;
+        this.cRecorridas = new int[nCiudades-1];
+        for (int i = 0; i < this.cRecorridas.length; i++) {
+            this.cRecorridas[i] = 0;
+        }
+    }
+    
+    
+    
+    
+}
